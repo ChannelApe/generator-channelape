3f8decb3e49f840b2c6f0db9ba0087327f2a5c6a
Craig Simko <csimko@channelape.com>
2018-07-03 15:24:20 -0400

    CA-3267 Linting Stryker

--- generators/app/templates/stryker.conf.js
+++ generators/app/templates/stryker.conf.js
@@ -1,23 +1,23 @@
-module.exports = function(config) {
+module.exports = function (config) {
   config.set({
     files: [
-      "test/**/*.ts",
-      "src/**/*.ts",
-      "!src/types/*d.ts",
-      "!src/index.ts"
+      'test/**/*.ts',
+      'src/**/*.ts',
+      '!src/types/*d.ts',
+      '!src/index.ts'
     ],
-    testRunner: "mocha",
-    mutator: "typescript",
-    transpilers: ["typescript"],
-    reporter: ["clear-text", "progress", "html"],
-    testFramework: "mocha",
-    coverageAnalysis: "off",
-    tsconfigFile: "tsconfig.json",
-    thresholds: { high: 90, low: 70, break: 20 },
+    testRunner: 'mocha',
+    mutator: 'typescript',
+    transpilers: ['typescript'],
+    reporter: ['clear-text', 'progress', 'html'],
+    testFramework: 'mocha',
+    coverageAnalysis: 'off',
+    tsconfigFile: 'tsconfig.json',
+    thresholds: {high: 90, low: 70, break: 20},
     mutate: [
-      "src/**/*.ts",
-      "!src/types/*d.ts",
-      "!src/index.ts"
+      'src/**/*.ts',
+      '!src/types/*d.ts',
+      '!src/index.ts'
     ]
   });
 };
